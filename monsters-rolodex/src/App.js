b73233f2fe570905ad318a65bccc1addc26a7df8
lgope <ilakshmangope@gmail.com>
2019-12-11 01:30:04 +0600

    Class Component 🗽©

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -2,28 +2,50 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-Funtion Component
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          🙏 Edit <code>src/App.js</code> and save to reload. 🙋‍♂️
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Funtion Component
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           🙏 Edit <code>src/App.js</code> and save to reload. 🙋‍♂️
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// Class Component
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            🙏 Edit <code>src/App.js</code> and save to reload. Hi 🙋‍♂️
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
-
-
 
 export default App;
