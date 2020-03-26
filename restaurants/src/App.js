1b33ddff08322156bc14ec1b29c0d59b5387ac57
lgope <ilakshmangope@gmail.com>
2020-03-26 21:40:32 +0600

    component-2 exercise ❄

--- restaurants/src/App.js
+++ restaurants/src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/Menu.component';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color='primary'>
-        <div className='container'>
-          <NavbarBrand href='/'>Restaurant</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark color='primary'>
+          <div className='container'>
+            <NavbarBrand href='/'>Restaurant</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
 }
 
 export default App;
