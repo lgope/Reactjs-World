a6949f8f1418f86dd523300b019d526f71bd95ed
lgope <ilakshmangope@gmail.com>
2020-04-17 01:36:14 +0600

    image gallery simple react app ❄

---
+++ image-gallery/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import ImageCard from './components/imageCard';
+import ImageSearch from './components/imageSearch';
+
+function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+
+  return (
+    <div className='container mx-auto'>
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && <h1 className='text-5xl text-center mx-auto mt-32'>No Images Found 😟. Try Diffenet! 🙂</h1>}
+      {isLoading ? (
+        <h1 className='text-6xl text-center mx-auto mt-32'>Loading..</h1>
+      ) : (
+        <div className='grid grid-cols-3 gap-4'>
+          {images.map((image) => {
+            return <ImageCard key={image.id} image={image} />;
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
