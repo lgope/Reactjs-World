c6b009ba40b5de0fcdceef7c68b825a1de4dc699
lgope <ilakshmangope@gmail.com>
2020-05-03 13:43:02 +0600

    multistep form initial commit 🚀

--- multi-step-form/src/App.js
+++ multi-step-form/src/App.js
@@ -1,26 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
-
-function Button(props) {
-  const handleClick = () => props.onClickFunction(props.increment);
-  return <button onClick={handleClick}>+{props.increment}</button>;
-}
-
-function Display(props) {
-  return <div>{props.message}</div>;
-}
+import UserForm from './components/UserForm.component';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const incrementCounter = (increment) => setCounter(counter + increment);
-
   return (
     <div className='App'>
-      <Button onClickFunction={incrementCounter} increment={1} />
-      <Button onClickFunction={incrementCounter} increment={5} />
-      <Button onClickFunction={incrementCounter} increment={10} />
-      <Button onClickFunction={incrementCounter} increment={100} />
-      <Display message={counter} />
+      <UserForm />
     </div>
   );
 }
---
+++ multi-step-form/src/components/Confirm.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Confirm() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Confirm;
