a5a74b985b22fb44040384f6723642a8a0da11d8
lgope <ilakshmangope@gmail.com>
2020-05-20 14:48:38 +0600

    image scrolling 🚀

--- inifite-image-scroll/client/src/App.js
+++ inifite-image-scroll/client/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Images from './components/Images.component';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <div id='root'>
+        <div className='hero is-fullheight is-bold is-info'>
+          <div className='hero-body'>
+            <div className='container'>
+              <div className='header content'>
+                <h2 className='subtitle is-6'>Code Challenge #16</h2>
+                <h1 className='title is-1'>
+                  Infinite Scroll Unsplash Code Challenge
+                </h1>
+              </div>
+              <Images />
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
 
