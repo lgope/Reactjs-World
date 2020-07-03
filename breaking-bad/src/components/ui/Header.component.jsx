b81e6bcb67168257846306273e46dd85eac62dea
lgope <ilakshmangope@gmail.com>
2020-07-03 23:34:41 +0600

    breaking bad react app

---
+++ breaking-bad/src/components/ui/Header.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../images/logo.png';
+
+const Header = () => {
+  return (
+    <header className='center'>
+      <img src={logo} alt='logo' />
+    </header>
+  );
+};
+
+export default Header;
---
+++ breaking-bad/src/components/ui/Spinner.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import spinner from '../../images/spinner.gif';
+
+const Spinner = () => {
+  return (
+    <img
+      src={spinner}
+      style={{ width: '200px', margin: 'auto', display: 'block' }}
+      alt='Loading spinner'
+    />
+  );
+};
+
+export default Spinner;
