14b085e5237bf7739f4d49ed867838f9a28ddf83
Lakshman <ilakshmangope@gmail.com>
2020-09-29 23:28:39 +0600

    Create alert.js

---
+++ alert.js
@@ -0,0 +1,26 @@
+import Swal from 'sweetalert2';
+
+// type is 'success' or 'error'
+export const showAlert = (type, msg) => {
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'top',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    onOpen: toast => {
+      toast.addEventListener('mouseenter', Swal.stopTimer);
+      toast.addEventListener('mouseleave', Swal.resumeTimer);
+    },
+  });
+
+  Toast.fire({
+    icon: type,
+    title: msg,
+  });
+};
+
+
+// import sample
+import { showAlert } from '../alert';
+showAlert('success', 'Updated Successfully!');
