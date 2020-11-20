97f8eddb8c17cd7c7a6b1148a6c8a5e7c57b9d43
lgope <ilakshmangope@gmail.com>
2020-11-20 23:10:05 +0600

    redux

--- redux-crud/src/app/store.js
+++ redux-crud/src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-import userReducer from '../features/user/userSlice';
+import userReducer from '../features/user/userSlice'; 
 
 export default configureStore({
   reducer: {
